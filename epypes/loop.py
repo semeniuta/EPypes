3d2bb122fc1705909761930e042cb6691d9bd655
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-02-15 13:23:05 +0100

    Simplified EventLoop

--- epypes/loop.py
+++ epypes/loop.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 
-from epypes.commons import GenericObject
-
-import threading
+from threading import Thread
 import sys
 ver = sys.version_info[:2]
 if ver[0] == 2:
@@ -10,32 +8,27 @@ if ver[0] == 2:
 else:
     import queue
 
-def basic_eventloop(callback_node, token_q, stop_q):
-    while True:
-        if not stop_q.empty():
-            stop_q.get()
-            break
-
-        if not token_q.empty():
-            token = token_q.get()
-            callback_node.run(token)
-
-class EventLoop(GenericObject):
+class EventLoop(Thread):
 
     def __init__(self, q, callback_node):
-        name = 'In{}'.format(callback_node.__class__.__name__)
-        GenericObject.__init__(self, name)
 
         self._callback_node = callback_node
         self._token_q = q
         self._stop_q = queue.Queue()
 
-        t_args = (self._callback_node, self._token_q, self._stop_q)
-        self._thread = threading.Thread(target=basic_eventloop, args=t_args)
+        Thread.__init__(self, target=self._eventloop)
 
-    def start(self):
-        self._thread.start()
+    def _eventloop(self):
+
+        while True:
+
+            if not self._stop_q.empty():
+                self._stop_q.get()
+                break
+
+            if not self._token_q.empty():
+                token = self._token_q.get()
+                self._callback_node.run(token)
 
     def request_stop(self):
         self._stop_q.put(None)
-        self._thread.join()
